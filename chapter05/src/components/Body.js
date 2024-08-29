9d1f46ac980a0fb3d0b1da5c9638772b434b38d2
kimzini <rlawlsdl11111@gmail.com>
2024-08-29 23:50:13 +0900

    구조 분해 할당으로 여러 개의 값 사용하기

--- chapter05/src/components/Body.js
+++ chapter05/src/components/Body.js
@@ -1,6 +1,8 @@
-function Body(props) {
-    console.log(props);
-    return <div className="body">{props.name}은 {props.location}에 거주합니다</div>;
+function Body({name, location}) {
+    console.log(name, location);
+    return (
+        <div className="body">{name}은 {location}에 거주합니다</div>
+    );
 }
 
 export default Body;
