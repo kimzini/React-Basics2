26e00a59e07298e16c7bda23dc55d711e71399c3
kimzini <rlawlsdl11111@gmail.com>
2024-08-29 23:59:57 +0900

    props로 컴포넌트 전달하기

--- chapter05/src/App.js
+++ chapter05/src/App.js
@@ -3,17 +3,23 @@ import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
 
+function ChildComp() {
+  return <div>child component</div>
+}
+
 function App() {
   const BodyProps = {
     name: "김진희",
     location: "부천시",
-    favorList: ["치킨!!!!!"],
+    favorList: ["치킨", "빵"],
   };
 
   return (
     <div className="App">
       <Header />
-      <Body {...BodyProps}/>
+      <Body>
+        <ChildComp />
+      </Body>
       <Footer />
     </div>
   );
--- chapter05/src/components/Body.js
+++ chapter05/src/components/Body.js
@@ -1,15 +1,10 @@
-function Body({name, location, favorList}) {
-    console.log(name, location, favorList);
+function Body({children}) {
+    console.log(children);
     return (
         <div className="body">
-            {name}은 {location}에 거주합니다<br />
-            {favorList.length}개의 음식을 좋아합니다.
+           {children}
         </div>
     );
 }
-
-Body.defaultProps = {
-    favorList: [],
-};
 
 export default Body;
