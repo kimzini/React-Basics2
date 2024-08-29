4697d6d5691e081248ab38a09644f4941c1059c7
kimzini <rlawlsdl11111@gmail.com>
2024-08-29 23:38:59 +0900

    컴포넌트 만들기

---
+++ chapter05/src/components/Body.js
@@ -0,0 +1,15 @@
+import "./Body.css";
+
+function Body() {
+    const objA = {
+        a: 1,
+        b: 2,
+    }
+    return (
+        <div className = "body">
+            <h1>body</h1>
+        </div>
+    );
+}
+
+export default Body;
